01f0ce660122060cd763f8998bdf5ad9a9e8fff8
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2024-08-08 16:47:10 -0300

    JavaScript

---
+++ Atividades para treinar/JS/Primeira Aula/meu_script.js
@@ -0,0 +1,12 @@
+document.getElementById("nome").value = "oi"
+
+// comentario de uma linha
+
+/* 
+    Comentário de múltiplas linhas
+    Esse tipo de comentário permite
+    que várias linhas sejam encapsuladas
+    em um mesmo comentário
+    
+*/
+
