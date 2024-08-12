5cc9a7fa4cc8df7b971d763f2a10f1e457142ae3
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2024-08-12 16:32:27 -0300

    Aula JavaScript

---
+++ Atividades para treinar/JS/Desafios/desafio1.js
@@ -0,0 +1,20 @@
+var a = 10
+var b = 20
+var c = null
+
+document.write("Variável A: " + a)
+document.write("<br>")
+document.write("Variável B: " + b)
+document.write("<br>")
+document.write("Variável C: " + c)
+document.write("<br>")
+document.write("<br>")
+
+var c = a
+var a = b
+
+
+document.write("Variável A: " + a)
+document.write("<br>")
+document.write("Variável B: " + c)
+document.write("<br>")
---
+++ Atividades para treinar/JS/Pratica Variaveis/variaveis2.js
@@ -0,0 +1,14 @@
+// null 
+var teste = null
+
+// undefined
+var teste2
+
+console.log(teste)
+
+teste = "Valor atualizado com sucesso!"
+console.log(teste)
+
+teste = 17.90
+console.log(teste)
+
